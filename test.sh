2fb52a68066906385d330a7305817e992ce39680
qwreey <me@qwreey.moe>
2024-08-05 02:45:05 +0900

    Add xfreerdp shadow

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo docker compose build ; sudo docker compose up -d
-sudo docker compose exec arch-docker bash
-sudo docker compose down
+docker compose build
+docker compose up -d
+docker compose exec arch-docker bash
+docker compose down
