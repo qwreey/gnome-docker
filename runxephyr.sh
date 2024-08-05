99514b732dd4b7947e14cec3ad2fc820e2908bdf
qwreey <me@qwreey.moe>
2024-08-05 16:57:33 +0900

    Remove Xephyr support

--- runxephyr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#-zaphod +iglx
-#-screen 1200x800x24 -dpi
-
-# Xephyr -br -ac -noreset -glamor +extension DAMAGE +extension MIT-SHM +extension RANDR +extension RENDER +extension GLX +extension DPMS -screen 1200x800 :1001
-#  +xinerama +extension XINERAMA -schedInterval
-
-Xephyr -schedInterval 16 +iglx -v -listen unix -fakescreenfps 60 -ac -reset -glamor -noxv +extension COMPOSITE +extension RANDR +extension RENDER +extension DAMAGE +extension MIT-SHM +extension GLX +extension DPMS -screen 1200x800x24x60 :1001
