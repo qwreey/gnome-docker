2281764b320a43898eb0147f6c03a90955d99275
qwreey <me@qwreey.moe>
2024-09-11 19:02:19 +0900

    Update dev script and restart option

--- runvnc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec vncviewer NoJPEG=1 CompressLevel=0 PreferredEncoding=Raw SecurityTypes=None ./host/vncsocket
--- test.sh
+++ test.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
 
-docker compose build
-docker compose up -d
-docker compose exec gnome-docker journalctl -f
-docker compose down
+read -d '' INNER << EOF
+trap 'int' SIGINT
+
+docker compose -f docker-compose.yml up -d 2> /dev/null
+
+docker compose -f docker-compose.yml exec --no-TTY gnome-docker journalctl -f &
+LOG="\$!"
+
+while [ -e ./host/running ]; do
+    sleep 1
+done
+
+docker compose -f docker-compose.yml down --timeout 10 2> /dev/null
+wait \$LOG
+EOF
+
+sigint() {
+    [ -e "./host/running" ] && rm ./host/running
+    if [ ! -z "$VNCPID" ]; then
+        kill -INT "$VNCPID"
+        wait $VNCPID
+    fi
+    wait "$DOCKERPID"
+    exit 0
+}
+trap 'sigint' SIGINT
+
+touch ./host/running
+
+sudo bash -c "$INNER" &
+DOCKERPID="$!"
+
+while [ ! -e ./host/vncsocket ]; do
+    sleep 0.1
+done
+
+vncviewer NoJPEG=1 CompressLevel=0 PreferredEncoding=Raw SecurityTypes=None ./host/vncsocket &
+VNCPID="$!"
+
+wait "$VNCPID"
+VNCPID=""
+
+rm ./host/running
+wait "$DOCKERPID"
+
+exit 0
